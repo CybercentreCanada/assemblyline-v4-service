deeac3714738c5063fb553eee378f95cd9d216f5
Rushi <rushi.nathvani@165gc.onmicrosoft.com>
2019-08-16 11:23:42 -0400

    Use python to manage processes

---
+++ docker/v4_service_base/process_handler.py
@@ -0,0 +1,29 @@
+import time
+from subprocess import Popen
+
+if __name__ == '__main__':
+    # Start the two processes
+    rs_p = Popen(['python3', '-m', 'alv4_service.run_service'])
+    th_p = Popen(['python3', '-m', 'al_service_client.task_handler'])
+
+    while True:
+        rs_rc = rs_p.poll()
+        th_rc = th_p.poll()
+
+        # If both processes exited, then return the highest exit code
+        if rs_rc is not None and th_rc is not None:
+            print(f"run_service: exit({rs_rc}) | task_handler: exit({th_rc})")
+            exit(max(rs_rc, th_rc))
+
+        # Check and exit if run_service process exited
+        if rs_rc is not None:
+            print(f"run_service: exit({rs_rc})")
+            exit(rs_rc)
+
+        # Check and exit if task_handler process exited
+        if th_rc is not None:
+            print(f"task_handler: exit({th_rc})")
+            exit(th_rc)
+
+        # Wait 10 seconds before polling process status
+        time.sleep(10)
---
+++ docker/v4_service_base_dev/process_handler.py
@@ -0,0 +1,29 @@
+import time
+from subprocess import Popen
+
+if __name__ == '__main__':
+    # Start the two processes
+    rs_p = Popen(['python3', '/opt/alv4/alv4_service/alv4_service/run_service.py'])
+    th_p = Popen(['python3', '/opt/alv4/alv4_service_client/al_service_client/task_handler.py'])
+
+    while True:
+        rs_rc = rs_p.poll()
+        th_rc = th_p.poll()
+
+        # If both processes exited, then return the highest exit code
+        if rs_rc is not None and th_rc is not None:
+            print(f"run_service: exit({rs_rc}) | task_handler: exit({th_rc})")
+            exit(max(rs_rc, th_rc))
+
+        # Check and exit if run_service process exited
+        if rs_rc is not None:
+            print(f"run_service: exit({rs_rc})")
+            exit(rs_rc)
+
+        # Check and exit if task_handler process exited
+        if th_rc is not None:
+            print(f"task_handler: exit({th_rc})")
+            exit(th_rc)
+
+        # Wait 10 seconds before polling process status
+        time.sleep(10)
