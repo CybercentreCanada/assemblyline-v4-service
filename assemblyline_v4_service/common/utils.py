ee9ee87c632e0dc84b502455801b6e05fc460e20
Rushi <rushi.nathvani@165gc.onmicrosoft.com>
2019-12-03 09:45:49 -0500

    Added alarm_clock util

--- assemblyline_v4_service/common/utils.py
+++ assemblyline_v4_service/common/utils.py
@@ -12,3 +12,28 @@ def set_death_signal(sig=signal.SIGTERM):
     def process_control():
         return libc.prctl(1, sig)
     return process_control
+
+
+class TimeoutException(Exception):
+    pass
+
+
+class alarm_clock:
+    """A context manager that causes an exception to be raised after a timeout."""
+
+    def __init__(self, timeout):
+        self.timeout = timeout
+        self.alarm_default = signal.getsignal(signal.SIGALRM)
+
+    def __enter__(self):
+        def handler(signum, frame):
+            raise TimeoutException("Timeout")
+
+        signal.signal(signal.SIGALRM, handler)
+
+        signal.alarm(self.timeout)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        signal.alarm(0)
+        signal.signal(signal.SIGALRM, self.alarm_default)
