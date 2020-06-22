cabba3657457d3c8438e61e2adaa94a9ca31a2b4
cccs-kevin <kevin.hardy-cooper@cyber.gc.ca>
2020-06-22 08:36:49 -0400

    Changed how we handle path, made logic tweaks

--- assemblyline_v4_service/common/tag_reducer.py
+++ assemblyline_v4_service/common/tag_reducer.py
@@ -7,6 +7,7 @@ NUMBER_REGEX = re.compile("[0-9]*")
 ALPHA_REGEX = re.compile("[a-zA-Z]*")
 ALPHANUM_REGEX = re.compile("[a-zA-Z0-9]*")
 BASE64_REGEX = re.compile("(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?")
+DO_NOT_REDUCE = ["netloc", "hostname"]
 
 
 def reduce_uri_tags(uris=None) -> []:
@@ -42,7 +43,8 @@ def reduce_uri_tags(uris=None) -> []:
             # note that values of keys in dict will be in lists of length 1, which we don't want
             uri_dict["query"] = parse_qs(uri_dict["query"])
         if uri_dict["path"] != "":
-            uri_dict["path"] = os.path.split(uri_dict["path"])
+            # converting tuple to list
+            uri_dict["path"] = list(os.path.split(uri_dict["path"]))
 
         parsed_uris.append(uri_dict)
 
@@ -59,7 +61,7 @@ def reduce_uri_tags(uris=None) -> []:
             if parsed_uri == comparison_uri:
                 continue
             equal_keys = 0
-            max_tuple_len = 0
+            max_list_len = 0
             len_keys = 0
             difference = {}
             # now go through each key, and check for equality
@@ -68,23 +70,24 @@ def reduce_uri_tags(uris=None) -> []:
                 comp_val = comparison_uri[key]
 
                 # if equal, add to count of similar keys
-                if type(val) == tuple:
+                if type(val) == list:
                     val_len = len(val)
                     if val == comp_val:
                         equal_keys += val_len
                     else:
-                        difference[key] = tuple()
+                        difference[key] = dict()
                         comp_len = len(comp_val)
-                        max_tuple_len = max(val_len, comp_len)
-                        for item in range(max_tuple_len):
+                        max_list_len = max(val_len, comp_len)
+                        for item in range(max_list_len):
                             if item >= comp_len or item >= val_len:
                                 # bail!
                                 break
                             if val[item] == comp_val[item]:
                                 equal_keys += 1
                             else:
-                                difference[key] = difference[key] + (val[item],)
-                                difference[key] = difference[key] + (comp_val[item],)
+                                difference[key][item] = []
+                                difference[key][item].append(val[item])
+                                difference[key][item].append(comp_val[item])
 
                 elif type(val) == dict:
                     val_len = len(val)
@@ -106,38 +109,36 @@ def reduce_uri_tags(uris=None) -> []:
                                     difference[key][item].append(val[item])
                                 if comp_val.get(item):
                                     difference[key][item].append(comp_val[item])
-                else:  # Not dict or a tuple
+                else:  # Not dict or a list
                     if val == comp_val:
                         equal_keys += 1
                     else:
-                        difference[key] = comp_val
+                        difference[key] = []
+                        difference[key].append(val)
+                        difference[key].append(comp_val)
             # now find percentage similar
-            percentage_equal = equal_keys/(len(parsed_uri.keys())+max_tuple_len+len_keys)
+            percentage_equal = equal_keys/(len(parsed_uri.keys())+max_list_len+len_keys)
 
             # if percentage equal is > some value (say 90), then we can say that
             # urls are similar enough to reduce
-            if percentage_equal > 0.8:
+            if percentage_equal >= 0.80:
                 # So that we don't overwrite details
                 comparison_uri_copy = deepcopy(comparison_uri)
                 # somehow recognize where parameters are that match and replace them.
                 for item in difference.keys():
-                    if type(difference[item]) == dict:
-                        for key in difference[item].keys():
+                    # We don't want to replace the following:
+                    if item in DO_NOT_REDUCE:
+                        continue
+
+                    val = difference[item]
+                    if item == "query":
+                        for key in val.keys():
                             placeholders = []
                             # since each of these items is a list of lists
-                            for l in difference[item][key]:
+                            for l in val[key]:
                                 # use regex to determine the parameter type
                                 value = l[0]
-                                if NUMBER_REGEX.fullmatch(value):
-                                    placeholder = "${NUMBER}"
-                                elif ALPHA_REGEX.fullmatch(value):
-                                    placeholder = "${ALPHA}"
-                                elif ALPHANUM_REGEX.fullmatch(value):
-                                    placeholder = "${ALPHA_NUM}"
-                                elif BASE64_REGEX.fullmatch(value):
-                                    placeholder = "${BASE64}"
-                                else:
-                                    placeholder = "${UNKNOWN_TYPE}"
+                                placeholder = get_placeholder(value)
                                 placeholders.append(placeholder)
                             if len(set(placeholders)) == 1:
                                 # the same placeholder type is consistent with all values
@@ -146,12 +147,29 @@ def reduce_uri_tags(uris=None) -> []:
                             else:
                                 # the placeholder types vary
                                 comparison_uri_copy[item][key] = ",".join(placeholders)
-                    elif type(difference[item]) == tuple:
-                        # TODO: implement this section when the path changes
-                        pass
+                    elif item == "path":
+                        placeholders = {}
+                        for key in val.keys():
+                            placeholders[key] = []
+                            for list_item in val[key]:
+                                # if / exists, pop the rest out
+                                if list_item != "/" and list_item[0] == "/":
+                                    # use regex to determine the parameter type
+                                    placeholder = get_placeholder(list_item[1:])
+                                    placeholders[key].append("/"+placeholder)
+                                else:
+                                    placeholder = get_placeholder(list_item)
+                                    placeholders[key].append(placeholder)
+                        for key in placeholders.keys():
+                            if len(set(placeholders[key])) == 1:
+                                # the same placeholder type is consistent with all values
+                                # update the comparison_uri_copy value
+                                comparison_uri_copy[item][key] = list(set(placeholders[key]))[0]
+                            else:
+                                # the placeholder types vary
+                                comparison_uri_copy[item][key] = ",".join(set(placeholders[key]))
                     else:
-                        # TODO: implement this if domains or anything else changes
-                        pass
+                        comparison_uri_copy[item] = get_placeholder(val)
 
                 # now it's time to rejoin the parts of the url
                 reduced_uris.add(turn_back_into_uri(comparison_uri_copy))
@@ -166,7 +184,7 @@ def reduce_uri_tags(uris=None) -> []:
 
 def turn_back_into_uri(uri_parts) -> str:
     # turn the path back into a string
-    uri_parts["path"] = '/'.join(uri_parts["path"])
+    uri_parts["path"] = ''.join(uri_parts["path"])
     # turn the query back into a query string
     # first, remove the list wrappers
     for item in uri_parts["query"].keys():
@@ -179,6 +197,21 @@ def turn_back_into_uri(uri_parts) -> str:
                  uri_parts["query"], uri_parts["fragment"])
     real_url = urlunparse(uri_tuple)
     return real_url
+
+
+def get_placeholder(val: str) -> str:
+    if NUMBER_REGEX.fullmatch(val):
+        placeholder = "${NUMBER}"
+    elif ALPHA_REGEX.fullmatch(val):
+        placeholder = "${ALPHA}"
+    # Note that BASE64 Regex must happen before ALPHANUM regex or else ALPHANUM will hit on BASE64
+    elif BASE64_REGEX.fullmatch(val):
+        placeholder = "${BASE64}"
+    elif ALPHANUM_REGEX.fullmatch(val):
+        placeholder = "${ALPHA_NUM}"
+    else:
+        placeholder = "${UNKNOWN_TYPE}"
+    return placeholder
 
 
 REDUCE_MAP = {
@@ -196,50 +229,47 @@ if __name__ == "__main__":
         'network.static.ip': ['127.0.0.1'],
         'av.virus_name': ["bad_virus"],
         "network.static.uri": [
-            # Those fail to find similarities but should     -> K: change percentage match to 80%, now similarities are found
             "https://google.com?query=allo",
             "https://google.com?query=mon",
             "https://google.com?query=coco",
-            # Also they failed to be returned!   -> K: now they are returned
+            # These should reduce to 'https://google.com?query=${ALPHA}'
 
-            # Fail to reduce base of domain -- THIS IS GOOD!
             "https://abc.com?query=THISISATESTTHISISATEST",
             "https://def.com?query=THISISATESTTHISISATEST",
             "https://ghi.com?query=THISISATESTTHISISATEST",
+            # These shouldn't reduce due to unique domain/hostnames
 
-            # Fail to reduce paths... Do we want this??
-            "https://hello.com/patha/?query=THISISATESTTHISISATEST",
-            "https://hello.com/pathb/?query=THISISATESTTHISISATEST",
-            "https://hello.com/pathc/?query=THISISATESTTHISISATEST",
+            "https://hello.com/patha?query=THISISATESTTHISISATEST",
+            "https://hello.com/pathb?query=THISISATESTTHISISATEST",
+            "https://hello.com/pathc?query=THISISATESTTHISISATEST",
+            # These should reduce to "https://hello.com/${ALPHA}/?query=THISISATESTTHISISATEST"
 
-            # Fail to reduce paths that are all the same ...
-            "https://hello.com/path/?query=THISISATESTTHISISATEST1",
-            "https://hello.com/path/?query=THISISATESTTHISISATEST1",
-            "https://hello.com/path/?query=THISISATESTTHISISATEST1",
+            "https://bonjour.com/path?query=THISISATESTTHISISATEST1",
+            "https://bonjour.com/path?query=THISISATESTTHISISATEST1",
+            "https://bonjour.com/path?query=THISISATESTTHISISATEST1",
+            # These should reduce to "https://hello.com/path/?query=THISISATESTTHISISATEST1"
 
-            # Those succeed
-            "https://hello.com/path/?query=THISISATESTTHISISATEST1&rnd=123",
-            "https://hello.com/path/?query=THISISATESTTHISISATEST1&rnd=345",
-            "https://hello.com/path/?query=THISISATESTTHISISATEST1&rnd=567",
-
-            # Those fail to find similarities but should
-            "https://google.com?query=allo",
-            "https://google.com?query=mon",
-            "https://google.com?query=coco",
+            "https://hello.com/path?query=THISISATESTTHISISATEST1&rnd=123",
+            "https://hello.com/path?query=THISISATESTTHISISATEST1&rnd=345",
+            "https://hello.com/path?query=THISISATESTTHISISATEST1&rnd=567",
+            # These should reduced to "https://hello.com/path/?query=THISISATESTTHISISATEST1&rnd=${NUMBER}"
         ],
         "network.dynamic.uri": [
-            # Base 64 get detected as alphanum :(
-            "https://base64encodethis.com/path/?base64hash=c2hlemxsM3Iz",
-            "https://base64encodethis.com/path/?base64hash=YXNkZmVyamdhM2diMCBj",
-            "https://base64encodethis.com/path/?base64hash=IyQwMjN5di04ICEjIEApIGhcMmJ1cGY5NDMwYTIvNDEyMzIzNCBo"
+            "https://base64encodethis.com/path?base64hash=c2hlemxsM3Iz",
+            "https://base64encodethis.com/path?base64hash=YXNkZmVyamdhM2diMCBj",
+            "https://base64encodethis.com/path?base64hash=IyQwMjN5di04ICEjIEApIGhcMmJ1cGY5NDMwYTIvNDEyMzIzNCBo"
+            # These should be reduced to "https://base64encodethis.com/path/?base64hash=${BASE64}"
+            
             # Can't seem to find the type for this !?
-            "https://googlelicious.com/somepathother/?query=allo",
-            "https://googlelicious.com/somepathother/?query=mon",
-            "https://googlelicious.com/somepathother/?query=coco"
-            # Those succeed
-            "https://googlelicious.com/somepath/?rng=112431243",
-            "https://googlelicious.com/somepath/?rng=124312431243",
-            "https://googlelicious.com/somepath/?rng=22"
+            "https://googlelicious.com/somepathother?query=allo",
+            "https://googlelicious.com/somepathother?query=mon",
+            "https://googlelicious.com/somepathother?query=coco"
+            # These should be reduced to "https://googlelicious.com/somepathother/?query=${ALPHA}"
+            
+            "https://googlelicious.com/somepath?rng=112431243",
+            "https://googlelicious.com/somepath?rng=124312431243",
+            "https://googlelicious.com/somepath?rng=22"
+            # These should be reduced to "https://googlelicious.com/somepathother?rng=${NUMBER}"
             # Some weird third result is found which ends with a $
         ]
     }
