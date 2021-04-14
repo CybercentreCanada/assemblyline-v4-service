bc047351b9b69dbd493224fc0d91a0a56a1f7bd6
Dawson Brown <dawson.brown@cyber.gc.ca>
2021-04-14 10:39:05 -0400

    make MR change requests. add .vscode/ to .gitignore

--- assemblyline_v4_service/common/keytool_parse.py
+++ assemblyline_v4_service/common/keytool_parse.py
@@ -1,6 +1,7 @@
-import re
-from subprocess import Popen, PIPE, call
+from re import split
+from subprocess import Popen, PIPE
 from assemblyline.common.str_utils import safe_str
+from typing import List
 
 class Certificate():
     def __init__(self):
@@ -11,24 +12,23 @@ class Certificate():
         self.valid_from = ""
         self.valid_to = ""
 
-def keytool_printcert(cert_path: str):
+def keytool_printcert(cert_path: str) -> None:
     """
     This function runs the 'keytool -printcert' command against a provided file
 
     :param cert_path: A path to a certificate
     :return: the string output of 'keytool -printcert' or None
     """
-    stdout, stderr = Popen(["keytool", "-printcert", "-file", cert_path],
+    stdout, _ = Popen(["keytool", "-printcert", "-file", cert_path],
                                 stderr=PIPE, stdout=PIPE).communicate()
     stdout = safe_str(stdout)
 
-    if stdout:
-        if "keytool error" not in stdout:
-            return stdout
+    if stdout and "keytool error" not in stdout:
+        return stdout
 
     return None
 
-def certificate_chain_from_printcert(printcert: str):
+def certificate_chain_from_printcert(printcert: str) -> List[Certificate]:
     """
     This function parses the output of 'keytool -printcert' and creates a list of Certificate objects.
     The input to this function is the output of keytool_printcert
@@ -38,7 +38,7 @@ def certificate_chain_from_printcert(printcert: str):
     """
     certs = []
 
-    for cert_str in  re.split('Certificate\[\d+\]:', printcert): # split printcert output incase of certificate chain
+    for cert_str in split('Certificate\[\d+\]:', printcert): # split printcert output in case of certificate chain
         if cert_str == '':
             continue
         cert = Certificate()
@@ -50,10 +50,10 @@ def certificate_chain_from_printcert(printcert: str):
                 if len(country) != 1:
                     cert.country = country[1]
 
-            if "Issuer:" in line:
+            elif "Issuer:" in line:
                 cert.issuer = line.split(": ", 1)[1]
 
-            if "Valid from:" in line:
+            elif "Valid from:" in line:
                 cert.valid_from = line.split(": ", 1)[1].split(" until:")[0]
                 cert.valid_to = line.rsplit(": ", 1)[1]
 
