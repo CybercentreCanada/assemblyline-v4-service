6ad48edfbc9f5e334b49e7a569e108ea57a5b1bb
cccs-jh <63320703+cccs-jh@users.noreply.github.com>
2022-05-03 12:50:04 -0400

    Add function for getting tags starting from any node in the tree
    Refactored tag mapping into a function to better support mappings
    based on what the string starts with.
    Added function get_tree_tags which builds the tag map starting from
    the children of any node, and made ioc_tags call that.

--- assemblyline_v4_service/common/extractor/decode_wrapper.py
+++ assemblyline_v4_service/common/extractor/decode_wrapper.py
@@ -1,63 +1,78 @@
 from __future__ import annotations
 
 from collections import defaultdict
+from typing import Any, Optional
 
 from multidecoder.multidecoder import Multidecoder
 from multidecoder.query import invert_tree
 
 
-STATIC_MAP = {
+STATIC_TAG_MAP = {
     'network.domain': 'network.static.domain',
     'network.ip': 'network.static.ip',
     'network.url': 'network.static.uri'
 }
 
-DYNAMIC_MAP = {
+DYNAMIC_TAG_MAP = {
     'network.domain': 'network.dynamic.domain',
     'network.ip': 'network.dynamic.ip',
     'network.url': 'network.dynamic.uri',
 }
 
-COMMON_MAP = {
+TAG_MAP = {
     'network.email': 'network.email.address',
     'powershell.cmdlet': 'file.powershell.cmdlet',
-    # blacklist
-    'archive': 'file.string.blacklisted',
-    'av.name': 'file.string.blacklisted',
-    'cryptography': 'file.string.blacklisted',
-    'debugger.device.name': 'file.string.blacklisted',
-    'enable_content': 'file.string.blacklisted',
-    'environment.windows': 'file.string.blacklisted',
-    'event': 'file.string.blacklisted',
-    'guid': 'file.string.blacklisted',
-    'network.protocol': 'file.string.blacklisted',
-    'network.string': 'file.string.blacklisted',
-    'oid': 'file.string.blacklisted',
-    'privilege': 'file.string.blacklisted',
-    'ransomware.string': 'file.string.blacklisted',
-    'sandbox.id': 'file.string.blacklisted',
-    'security_identifier': 'file.string.blacklisted',
-    'vba.name': 'file.string.blacklisted',
-    'windows.registry': 'file.string.blacklisted'
 }
+
+BLACKLIST = {
+    'archive',
+    'av.name',
+    'cryptography',
+    'debugger.device.name',
+    'enable_content',
+    'environment.windows',
+    'event',
+    'guid',
+    'network.protocol',
+    'network.string',
+    'oid',
+    'privilege',
+    'ransomware.string',
+    'sandbox.id',
+    'security_identifier',
+    'vba.name',
+    'windows.registry'
+}
+
+
+def map_tag_type(tag_type: str, dynamic=False) -> Optional[str]:
+    if tag_type in TAG_MAP:
+        return TAG_MAP[tag_type]
+    if tag_type in STATIC_TAG_MAP:
+        return DYNAMIC_TAG_MAP[tag_type] if dynamic else STATIC_TAG_MAP[tag_type]
+    if tag_type in BLACKLIST:
+        return 'file.string.blacklisted'
+    if tag_type.startswith('api'):
+        return 'file.string.api'
+    if tag_type.startswith('filename'):
+        return 'file.name.extracted'
+    return None
+
+
+def get_tree_tags(tree: list[dict[str, Any]], dynamic=False) -> dict[str, set[bytes]]:
+    tags: dict[str, set[bytes]] = defaultdict(set)
+    nodes = invert_tree(tree)
+    for node in nodes:
+        tag_type = map_tag_type(node.type, dynamic)
+        if tag_type:
+            tags[tag_type].add(node.value)
+    return tags
 
 
 class DecoderWrapper():
     def __init__(self) -> None:
         self.multidecoder = Multidecoder()
-        self.map = COMMON_MAP
-        for label in self.multidecoder.analyzers.values():
-            if label.startswith('api'):
-                self.map[label] = 'file.string.api'
-            elif label.startswith('filename'):
-                self.map[label] = 'file.name.extracted'
 
     def ioc_tags(self, data: bytes, dynamic=False) -> dict[str, set[bytes]]:
-        self.map.update(DYNAMIC_MAP) if dynamic else self.map.update(STATIC_MAP)
-        tags: dict[str, set[bytes]] = defaultdict(set)
         tree = self.multidecoder.scan(data)
-        nodes = invert_tree(tree)
-        for node in nodes:
-            if node.type in self.map:
-                tags[self.map[node.type]].add(node.value)
-        return tags
+        return get_tree_tags(tree, dynamic)
