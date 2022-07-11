8734547d3e568d8dbcda077ab5c5b76a4f08ba78
cccs-rs <ryan.samaroo@cyber.gc.ca>
2022-07-11 16:50:40 +0000

    Allow HTTP-based downloads to support client certificates

--- assemblyline_v4_service/updater/helper.py
+++ assemblyline_v4_service/updater/helper.py
@@ -61,6 +61,7 @@ def url_download(source: Dict[str, Any], previous_update: int = None,
     pattern = source.get('pattern', None)
     username = source.get('username', None)
     password = source.get('password', None)
+    private_key = source.get('private_key', None)
     ca_cert = source.get('ca_cert', None)
     ignore_ssl_errors = source.get('ssl_ignore_errors', False)
     auth = (username, password) if username and password else None
@@ -78,6 +79,7 @@ def url_download(source: Dict[str, Any], previous_update: int = None,
     # Create a requests session
     session = requests.Session()
     session.verify = not ignore_ssl_errors
+    session.cert = private_key
 
     # Let https requests go through proxy
     if proxy:
