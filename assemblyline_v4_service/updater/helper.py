1300aa662cda927f40264ab4f8a62d33c5445f7c
cccs-rs <ryan.samaroo@cyber.gc.ca>
2022-07-15 17:46:00 +0000

    move outside of loop

--- assemblyline_v4_service/updater/helper.py
+++ assemblyline_v4_service/updater/helper.py
@@ -45,8 +45,8 @@ def filter_downloads(update_directory, pattern, default_pattern=".*") -> List[Tu
             if re.match(pattern, dirpath):
                 f_files.append((dirpath, get_sha256_for_file(make_archive(subdir, 'tar', root_dir=dirpath))))
 
-        if re.match(pattern, path_in_dir):
-            f_files.append((path_in_dir, get_sha256_for_file(make_archive(subdir, 'tar', root_dir=path_in_dir))))
+    if re.match(pattern, update_directory):
+        f_files.append((update_directory, get_sha256_for_file(make_archive(subdir, 'tar', root_dir=update_directory))))
 
     return f_files
 
