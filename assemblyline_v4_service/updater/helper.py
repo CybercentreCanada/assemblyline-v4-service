9bbb65541e7d76020126bd64202495131fde03bf
cccs-rs <ryan.samaroo@cyber.gc.ca>
2022-07-15 17:43:24 +0000

    allow cloned directory to be considered in updates

--- assemblyline_v4_service/updater/helper.py
+++ assemblyline_v4_service/updater/helper.py
@@ -37,13 +37,16 @@ def filter_downloads(update_directory, pattern, default_pattern=".*") -> List[Tu
         pattern = default_pattern
     for path_in_dir, subdirs, files in os.walk(update_directory):
         for filename in files:
-            filepath = os.path.join(update_directory, path_in_dir, filename)
+            filepath = os.path.join(path_in_dir, filename)
             if re.match(pattern, filepath) or re.match(pattern, filename):
                 f_files.append((filepath, get_sha256_for_file(filepath)))
         for subdir in subdirs:
-            dirpath = f'{os.path.join(update_directory, path_in_dir, subdir)}/'
+            dirpath = f'{os.path.join(path_in_dir, subdir)}/'
             if re.match(pattern, dirpath):
                 f_files.append((dirpath, get_sha256_for_file(make_archive(subdir, 'tar', root_dir=dirpath))))
+
+        if re.match(pattern, path_in_dir):
+            f_files.append((path_in_dir, get_sha256_for_file(make_archive(subdir, 'tar', root_dir=path_in_dir))))
 
     return f_files
 
