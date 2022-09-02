2913e63699b805a8a266459585fec7c4bca345af
Steve Garon <steve.garon@cyber.gc.ca>
2022-09-02 17:47:01 +0000

    Proper tests for ReasultSample

--- assemblyline_result_sample_service/test/test_resultsample.py
+++ assemblyline_result_sample_service/test/test_resultsample.py
@@ -1,20 +1,20 @@
 import os
+import pytest
 
-from pprint import pprint
 from assemblyline_result_sample_service.result_sample import ResultSample
 from assemblyline_v4_service.testing.helper import TestHelper
 
+# Force manifest location
+os.environ['SERVICE_MANIFEST_PATH'] = os.path.join(os.path.dirname(__file__), "..", "service_manifest.yml")
 
-def test_sample():
-    os.environ['SERVICE_MANIFEST_PATH'] = os.path.join(os.path.dirname(__file__), "..", "service_manifest.yml")
-    file_to_test = "87bcfad697742459387d54d11a090283027df97055decf1bef813610ab09ff80"
-    th = TestHelper(
-        ResultSample,
-        os.path.join(os.path.dirname(__file__), 'results'),
-        [os.path.join(os.path.dirname(__file__), 'samples')])
+# Setup folder locations
+RESULTS_FOLDER = os.path.join(os.path.dirname(__file__), 'results')
+SAMPLES_FOLDER = os.path.join(os.path.dirname(__file__), 'samples')
 
-    pprint(th._execute_sample(file_to_test, save=True))
+# Initialize test helper
+th = TestHelper(ResultSample, RESULTS_FOLDER, SAMPLES_FOLDER)
 
 
-if __name__ == "__main__":
-    test_sample()
+@pytest.mark.parametrize("sample", th.result_list())
+def test_sample(sample):
+    th.compare_sample_results(sample)
