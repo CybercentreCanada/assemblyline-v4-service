677af72c04fd0880a774383beae365ef71409322
cccs-rs <ryan.samaroo@cyber.gc.ca>
2023-03-14 18:25:38 +0000

    Use score to affect border coloring

--- assemblyline_v4_service/common/result.py
+++ assemblyline_v4_service/common/result.py
@@ -423,9 +423,9 @@ class TimelineSectionBody(SectionBody):
         return super().__init__(BODY_FORMAT.TIMELINE, body=[])
 
     def add_node(self, title: str, content: str, opposite_content: str,
-                 icon: str = None, signatures: List[str] = [], color: str = None) -> None:
+                 icon: str = None, signatures: List[str] = [], score: int = 0) -> None:
         self._data.append(dict(title=title, content=content, opposite_content=opposite_content,
-                          icon=icon, signatures=signatures, color=color))
+                          icon=icon, signatures=signatures, score=score))
 
 
 class ResultSection:
@@ -729,9 +729,10 @@ class ResultTimelineSection(TypeSpecificResultSection):
     def __init__(self, title_text: Union[str, List], **kwargs):
         super().__init__(title_text,  TimelineSectionBody(), **kwargs)
 
-    def add_node(self, title: str, content: str, opposite_content: str, icon=None, signatures=[], color=None) -> None:
+    def add_node(self, title: str, content: str, opposite_content: str, icon: str = None, signatures: List[str] = [],
+                 score: int = 0) -> None:
         self.section_body.add_node(title=title, content=content, opposite_content=opposite_content,
-                                   icon=icon, signatures=signatures, color=color)
+                                   icon=icon, signatures=signatures, score=score)
 
 
 class ResultMultiSection(TypeSpecificResultSection):
