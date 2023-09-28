cbb272b4ab326ca5892273aa1912db1009a27d39
cccs-jh <63320703+cccs-jh@users.noreply.github.com>
2023-09-28 14:50:09 -0400

    Improve test to better show insertion order preservation

--- test/test_result.py
+++ test/test_result.py
@@ -1,4 +1,5 @@
 import pytest
+
 from assemblyline_v4_service.common.result import _pack_tags, _unpack_tags
 
 
@@ -6,6 +7,8 @@ from assemblyline_v4_service.common.result import _pack_tags, _unpack_tags
     "tags",
     [
         {"file.string.blacklisted": ["pear", "apple", "banana"]},
+        {"file.string.blacklisted": ["first", "second"]},
+        {"file.string.blacklisted": ["second", "first"]},
     ],
 )
 def test_pack_unpack_identity(tags):
