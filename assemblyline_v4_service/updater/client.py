96d9f217dbc54ab2d76873de9518a9c92fb4ffa7
cccs-rs <62077998+cccs-rs@users.noreply.github.com>
2024-02-15 01:56:56 +0000

    Add a way to directly update hash lists instead of using API

--- assemblyline_v4_service/updater/client.py
+++ assemblyline_v4_service/updater/client.py
@@ -2,12 +2,46 @@ import os
 
 from assemblyline.common import forge
 from assemblyline.odm.models.signature import Signature as SignatureModel
+from assemblyline_core.badlist_client import BadlistClient
+from assemblyline_core.safelist_client import SafelistClient
 from assemblyline_client import Client4, get_client as get_AL_client
+from assemblyline_client.v4_client.module.badlist import Badlist as BadlistAPI
+from assemblyline_client.v4_client.module.safelist import Safelist as SafelistAPI
 from assemblyline_client.v4_client.module.signature import Signature as SignatureAPI
 
 from typing import Any, Dict, List, Union
 
+config = forge.get_config()
+
 SIGNATURE_UPDATE_BATCH = int(os.environ.get('SIGNATURE_UPDATE_BATCH', '1000'))
+
+
+class Badlist(BadlistAPI):
+    def __init__(self, connection, datastore=None):
+        super().__init__(connection)
+        if not datastore:
+            datastore = forge.get_datastore()
+        self.badlist_client = BadlistClient(datastore)
+
+    def add_update(self, badlist_object: dict):
+        return self.badlist_client.add_update(badlist_object)
+
+    def add_update_many(self, list_of_badlist_object):
+        return self.badlist_client.add_update_many(list_of_badlist_object)
+
+
+class Safelist(SafelistAPI):
+    def __init__(self, connection, datastore=None):
+        super().__init__(connection)
+        if not datastore:
+            datastore = forge.get_datastore()
+        self.safelist_client = SafelistClient(datastore)
+
+    def add_update(self, safelist_object):
+        return self.safelist_client.add_update(safelist_object)
+
+    def add_update_many(self, list_of_safelist_object):
+        return self.safelist_client.add_update_many(list_of_safelist_object)
 
 
 class Signature(SignatureAPI):
@@ -18,7 +52,8 @@ class Signature(SignatureAPI):
             self.datastore = forge.get_datastore()
         self.sync = False
 
-    def add_update_many(self, source: str, sig_type: str, data: List[Union[dict, SignatureModel]], dedup_name: bool = True) -> Dict[str, Any]:
+    def add_update_many(self, source: str, sig_type: str, data: List[Union[dict, SignatureModel]],
+                        dedup_name: bool = True) -> Dict[str, Any]:
         # This version of the API allows to sync signatures with the system by making direct changes to the datastore
         # Signatures that no longer exist at the source will be DISABLED to maintain active synchronicity,
         # but users can always re-deploy signatures if desired
@@ -51,12 +86,13 @@ class Signature(SignatureAPI):
             # Get the list of signatures that currently existing in the system for the source
             existing_signature_ids = set([
                 i.id for i in self.datastore.signature.stream_search(f"source:{source} AND type:{sig_type}", fl='id')
-                ])
+            ])
 
             # Find the signature IDs that don't exist at this source anymore and disable them
             for missing_signature_id in (existing_signature_ids - current_signature_ids):
                 missing_signature = self.datastore.signature.get(missing_signature_id)
-                if missing_signature.state_change_user in ['update_service_account', None] and missing_signature.status != 'DISABLED':
+                if missing_signature.state_change_user in [
+                        'update_service_account', None] and missing_signature.status != 'DISABLED':
                     # Only disable signature if it doesn't seem to be in use/altered by a (real) user
                     self.datastore.signature.update(missing_signature_id,
                                                     [(self.datastore.signature.UPDATE_SET, 'status', 'DISABLED'),
@@ -98,6 +134,8 @@ def get_client(server, auth=None, cert=None, debug=lambda x: None, headers=None,
     client = get_AL_client(server, auth, cert, debug, headers,
                            retries, silence_requests_warnings,
                            apikey, verify, timeout, oauth)
-    # Override Signature module with custom implementation
+    # Override module(s) with custom implementation
+    client.badlist = Badlist(client._connection, datastore=datastore)
+    client.safelist = Safelist(client._connection, datastore=datastore)
     client.signature = Signature(client._connection, datastore=datastore)
     return client
