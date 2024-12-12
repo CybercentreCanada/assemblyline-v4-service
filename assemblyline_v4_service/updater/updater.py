d24cb92d2131ed47cd23705b514a4869c26ff83a
cccs-rs <62077998+cccs-rs@users.noreply.github.com>
2024-12-12 13:29:11 -0500

    Update assemblyline_v4_service/updater/updater.py

--- assemblyline_v4_service/updater/updater.py
+++ assemblyline_v4_service/updater/updater.py
@@ -407,8 +407,10 @@ class ServiceUpdater(ThreadedCoreBase):
                         # Enable syncing if the source specifies it
                         self.client.sync = source.get('sync', False)
                         # Override classfication of signatures if specified
-                        self.client.classification_override = default_classification \
-                            if source.get('override_classification', False) else None
+                        # Reset client back to original classification state between updates
+                        self.client.classification_override = None
+                        if source.get('override_classification', False):
+                            self.client.classification_override = default_classification
 
                         self.push_status("UPDATING", "Pulling..")
                         output = None
