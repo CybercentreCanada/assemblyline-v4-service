5a2919309c0e75e678251d39e09bccfeaca626da
cccs-rs <62077998+cccs-rs@users.noreply.github.com>
2025-02-10 14:36:42 +0000

    Fix storing the response for GETs

--- assemblyline_v4_service/updater/helper.py
+++ assemblyline_v4_service/updater/helper.py
@@ -136,7 +136,7 @@ def url_download(source: Dict[str, Any], previous_update: int, logger: Logger, o
                     headers = {'If-Modified-Since': previous_update}
 
             if fetch_method == 'get':
-                session.get(uri, auth=auth, headers=headers, proxies=proxies, stream=True)
+                response = session.get(uri, auth=auth, headers=headers, proxies=proxies, stream=True)
             elif fetch_method == 'post':
                 json = source.get('post_data') or None
                 response = session.post(uri, auth=auth, headers=headers, proxies=proxies, json=json, stream=True)
