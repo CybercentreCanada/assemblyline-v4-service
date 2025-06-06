58d80d925cac65ee8ae023fd1358560799b44bfb
cccs-rs <62077998+cccs-rs@users.noreply.github.com>
2025-06-06 17:00:13 +0000

    Modify test to use the client for adding signatures to system

--- test/test_updater/test_updater.py
+++ test/test_updater/test_updater.py
@@ -1,14 +1,21 @@
+import json
 import os
 import random
-import pytest
 import tempfile
 
+import pytest
+from assemblyline_v4_service.updater.updater import (
+    SERVICE_NAME,
+    SIGNATURES_META_FILENAME,
+    SOURCE_STATUS_KEY,
+    SOURCE_UPDATE_TIME_KEY,
+    ServiceUpdater,
+)
 
 from assemblyline.odm.models.service import Service
 from assemblyline.odm.models.service_delta import ServiceDelta
 from assemblyline.odm.models.signature import Signature
 from assemblyline.odm.randomizer import random_model_obj
-from assemblyline_v4_service.updater.updater import ServiceUpdater, SERVICE_NAME, SOURCE_STATUS_KEY, SOURCE_UPDATE_TIME_KEY
 
 os.environ['SERVICE_PATH'] = SERVICE_NAME
 
@@ -35,7 +42,7 @@ def updater():
 
 
 @pytest.fixture()
-def initialized_updater(updater):
+def initialized_updater(updater: ServiceUpdater):
     # Sync settings from the datastore
     updater._pull_settings()
 
@@ -48,11 +55,11 @@ def initialized_updater(updater):
 
 
 
-def test_init(updater):
+def test_init(updater: ServiceUpdater):
     # Ensure updater can be initialized when provided the expected environment variables
     assert updater.updater_type == SERVICE_NAME
 
-def test_pull_settings(updater):
+def test_pull_settings(updater: ServiceUpdater):
     # When first initialized, the updater doesn't have any information about the service
     assert not updater._service
 
@@ -61,26 +68,26 @@ def test_pull_settings(updater):
 
     assert updater._service and updater._service.name == SERVICE_NAME
 
-def test_inventory_check(initialized_updater):
+def test_inventory_check(initialized_updater: ServiceUpdater):
     # There is currently no updates being distributed so it's too early to perform an inventory check
-    assert not initialized_updater._update_dir and initialized_updater._inventory_check() == False
+    assert not initialized_updater._update_dir and not initialized_updater._inventory_check()
 
     # Simulate a update directory (with missing updates)
     with tempfile.TemporaryDirectory() as dir:
         initialized_updater._update_dir = dir
 
         # If the other thread performing source updates is running, then we have to wait until it's completed before tasking missing sources
-        assert initialized_updater.source_update_flag.is_set() == True and initialized_updater._inventory_check() == False and not initialized_updater.update_queue.qsize()
+        assert initialized_updater.source_update_flag.is_set() and not initialized_updater._inventory_check() and not initialized_updater.update_queue.qsize()
 
         # Simulate if the source update thread is idle
         initialized_updater.source_update_flag.clear()
 
         # Expect the inventory check to fail and there should be tasking for all the sources to be fetched
-        assert initialized_updater._inventory_check() == False and initialized_updater.update_queue.qsize() == len(initialized_updater._service.update_config.sources)
+        assert not initialized_updater._inventory_check() and initialized_updater.update_queue.qsize() == len(initialized_updater._service.update_config.sources)
 
         # Simulate a scenario where the inventory check will pass (there is at least one update available for distribution)
-        with tempfile.NamedTemporaryFile(dir=dir, suffix=random.choice(initialized_updater._service.update_config.sources).name) as update:
-            assert initialized_updater._inventory_check() == True
+        with tempfile.NamedTemporaryFile(dir=dir, suffix=random.choice(initialized_updater._service.update_config.sources).name):
+            assert initialized_updater._inventory_check()
 
 @pytest.mark.parametrize("generates_signatures", [True, False])
 def test_do_local_update(initialized_updater, generates_signatures):
@@ -90,13 +97,14 @@ def test_do_local_update(initialized_updater, generates_signatures):
 
         # Populate some signatures for each source
         sources = [s.name for s in initialized_updater._service.update_config.sources]
+        signatures = []
         for update in sources:
             sig = random_model_obj(Signature)
             sig.source = update
             sig.type = SERVICE_NAME
             sig.status = "DEPLOYED"
-            initialized_updater.datastore.signature.save(sig.signature_id, sig)
-        initialized_updater.datastore.signature.commit()
+            signatures.append(sig)
+            initialized_updater.client.signature.add_update_many(update, initialized_updater.updater_type, [sig])
 
         # Perform local update
         initialized_updater.do_local_update()
@@ -114,7 +122,14 @@ def test_do_local_update(initialized_updater, generates_signatures):
         assert os.path.exists(service_updates) and \
             all([os.path.exists(os.path.join(service_updates, source)) for source in sources])
 
-def test_do_source_update(initialized_updater):
+        # Check that all signatures can be found in signature meta map
+        with open(os.path.join(initialized_updater._update_dir, SIGNATURES_META_FILENAME)) as meta_file:
+            sig_meta = json.load(meta_file)
+
+            for s in initialized_updater.datastore.signature.stream_search(f"type:{SERVICE_NAME}", fl="signature_id"):
+                assert s.signature_id in sig_meta
+
+def test_do_source_update(initialized_updater: ServiceUpdater):
     source = initialized_updater._service.update_config.sources[0]
     update_data_hash = initialized_updater.update_data_hash
     def task_source_update(field, value):
@@ -130,7 +145,7 @@ def test_do_source_update(initialized_updater):
 
         # If a source is disabled, we'll skip until it's re-enabled
         update_time, update_state = task_source_update("enabled", False)
-        assert update_time == None and update_state["message"] == "Skipped."
+        assert update_time is None and update_state["message"] == "Skipped."
 
         update_time, update_state = task_source_update("enabled", True)
         assert update_time > 0 and update_state["message"] == "Signature(s) Imported."
